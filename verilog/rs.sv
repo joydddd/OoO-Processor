596793058576dc4a942c5ecd423badf913b2afec
Bruce Wei <brucewei@umich.edu>
2021-03-08 02:06:13 -0500

    Issue_ready check 1

--- verilog/rs.sv
+++ verilog/rs.sv
@@ -12,11 +12,54 @@ module RS(
 );
 
 RS_IN_PACKET [2**`RS-1:0]        rs_entries;
-
+logic [2**`RS-1:0]             issue_ready;
 `ifdef TEST_MODE
     assign rs_entries_display = rs_entries;
 `endif
 
 // TODO: design
-
+always_comb begin
+    for (int i = 0; i < 2**`RS; i++) begin
+        if (issue_ready[i] == 0 && rs_entries[i].reg1_ready && rs_entries[i].reg2_ready) begin
+            case(rs_entries.fu_sel)
+                ALU_1: begin
+                    if (fu_ready.alu_1 == 1) begin
+                        issue_ready[i] = 1;
+                    end
+                    else if (fu_ready.alu_2 == 1) begin
+                        rs_entries.fu_sel = ALU_2;
+                        issue_ready[i] = 1;
+                    end
+                    else if (fu_ready.alu_3 == 1) begin
+                        rs_entries.fu_sel = ALU_3;
+                        issue_ready[i] = 1;
+                    end
+                end
+                LS_1: begin
+                    if (fu_ready.storeload_1 == 1) begin
+                        issue_ready[i] = 1;
+                    end
+                    else if (fu_ready.storeload_2 == 1) begin
+                        rs_entries.fu_sel = LS_2;
+                        issue_ready[i] = 1;
+                    end
+                end
+                MULT_1: begin
+                    if (fu_ready.mult_1 == 1) begin
+                        issue_ready[i] = 1;
+                    end
+                    else if (fu_ready.mult_2 == 1) begin
+                        rs_entries.fu_sel = MULT_2;
+                        issue_ready[i] = 1;
+                    end
+                end
+                BRANCH: begin
+                    if (fu_ready.branch == 1) begin
+                        issue_ready[i] = 1;
+                    end
+                end
+            endcase
+        end
+    end
+end
 endmodule
